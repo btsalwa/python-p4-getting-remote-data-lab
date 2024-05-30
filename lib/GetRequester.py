13602142b488b84b678449b89ecce42ba4ee285a
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-05-30 16:17:53 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,17 @@ import requests
 import json
 
 class GetRequester:
+    
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.content
         pass
 
     def load_json(self):
+        response_body = self.get_response_body()
+        return json.loads(response_body)
         pass
